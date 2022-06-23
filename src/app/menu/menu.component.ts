f031658258cf8cc0f2a45bdf95eb5545b1a801d3
Markus Falk <m.falk@meddevo.com>
2022-06-23 09:45:31 +0200

    mobile styles

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,14 +1,15 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
-import {ServerModel} from "../server/server.model";
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { ServerModel } from '../server/server.model';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
 })
 export class MenuComponent implements OnInit {
   @Output() emitToParent = new EventEmitter<ServerModel[]>();
-  servers: ServerModel[] = [{name: 'Beispiel Server 1'}, {name: 'Beispiel Server 2'}, {name: 'Beispiel Server 3'}];
+
+  servers: ServerModel[] = [];
   nameIndex: string[] = [];
   buttonDissabled: boolean = true;
   showError: boolean = false;
@@ -17,19 +18,17 @@ export class MenuComponent implements OnInit {
   setInputValue: string = '';
 
   onInput(inputValue: string) {
-    for (let i = 0; i < this.servers.length; i ++) {
+    for (let i = 0; i < this.servers.length; i++) {
       this.nameIndex[i] = this.servers[i].name;
     }
 
-    if(inputValue === ''){
+    if (inputValue === '') {
       this.buttonDissabled = true;
-    }
-    else if (this.nameIndex.includes(inputValue)) {
+    } else if (this.nameIndex.includes(inputValue)) {
       this.buttonDissabled = true;
-      this.errorMsg = "name already exists dumbass";
+      this.errorMsg = 'name already exists dumbass';
       this.showError = true;
-    }
-    else {
+    } else {
       this.buttonDissabled = false;
       this.showError = false;
       this.showCreation = false;
@@ -37,7 +36,7 @@ export class MenuComponent implements OnInit {
   }
 
   onCreateServer(name: string) {
-    const server: ServerModel = {name: name};
+    const server: ServerModel = { name: name };
     this.servers.push(server);
     this.setInputValue = '';
     this.buttonDissabled = true;
